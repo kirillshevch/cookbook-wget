260bec962cc00543cf6b094e8b281231d6998539
Kirill Shevchenko <kirills167@gmail.com>
2016-11-09 11:58:12 +0200

    init

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'wget'
+maintainer       "Kirill Shevchenko"
+maintainer_email "kirills167@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures wget"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+depends          "libidn", ">= 0.0.1"
+depends          "libssl", ">= 0.0.1"
